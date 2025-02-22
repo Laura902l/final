cfed601f402618593518b6e41b19e6bf2167db9a
kamilakazikhanova <anarab902@gmail.com>
2025-02-22 16:20:13 +0500

    Add all files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginForm from './component/login/LoginForm';
+
+function App() {
+  return (
+    <div>
+     <LoginForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Header.jsx
@@ -0,0 +1,13 @@
+import image from '/Users/kamilakaz77/Desktop/Новая папка 2/p/vite-project/src/assets/react.svg'
+
+export default function Header() {
+  return (
+    <header>
+      <img src={image} alt="" />
+  
+        <span>SchoolRank</span>
+
+
+    </header>
+  )
+}
